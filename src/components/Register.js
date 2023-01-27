d3ed5c2ce3f49abbc13bff1ccf577c108c7a8abf
Michelle Li <mli6@andrew.cmu.edu>
2023-01-27 15:03:53 -0500

    update some reg info

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,17 +6,16 @@ const register = () => {
       <div className="register-content">
         <div className="section-header">Registration</div>
         <p className="register-update">
-          We're still finalizing the details for AWAP 2023; stay tuned for
-          updates!
+          {/* We're still finalizing the details for AWAP 2023; stay tuned for
+          updates! */}
         </p>
         <div className="register-box">
           <p className="register-description">
-            Date: 2/17 - 2/18<br></br>
+            Date: 2/17 5 PM - 2/18 5 PM<br></br>
             Location: Rashid/Simmons<br></br>
-            Format: TBD<br></br>
-            Prize money: TBD<br></br>
-            Registration opens: TBD<br></br>
-            Eligibility: CMU students
+            Eligibility: CMU students<br></br>
+            Registration Deadline: <br></br>
+            Team Size: Up to 4
           </p>
         </div>
         <section>
