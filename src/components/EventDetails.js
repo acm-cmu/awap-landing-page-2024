79568594cb4af9446d48d6886cfb2202d0c4f2a1
Jeffery John <jefferyjohn2000@gmail.com>
2023-02-16 02:12:39 -0500

    improve mobile responsiveness

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -15,7 +15,7 @@ const EventDetails = () => {
             Start: Friday, 2/17, 5 PM @ GHC 4401 Rashid Auditorium<br></br>
             End: Saturday, 2/18 @ 5 PM @ TEP 1101 Simmons Auditorium<br></br>
           </p>
-          <iframe src="https://discord.com/widget?id=1074365724400832532&theme=dark" width="500" height="350" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+          <iframe src="https://discord.com/widget?id=1074365724400832532&theme=dark" width="102%" height="75%" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         </div>
         <section>
           <div className="line">
