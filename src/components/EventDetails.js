cc56083620c64047dc7cc953b466d93fade03393
Jeffery John <jefferyjohn2000@gmail.com>
2023-02-17 22:04:38 -0500

    update meta tags for day of

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -12,10 +12,17 @@ const EventDetails = () => {
         </p>
         <div className="register-box">
           <p className="register-description">
-            Start: Friday, 2/17, 5 PM @ GHC 4401 Rashid Auditorium<br></br>
-            End: Saturday, 2/18 @ 5 PM @ TEP 1101 Simmons Auditorium<br></br>
+            Start: Now - Happy hacking!<br></br>
+            End: Tomorrow, 5 PM @ TEP 1101 Simmons Auditorium!<br></br>
           </p>
-          <iframe src="https://discord.com/widget?id=1074365724400832532&theme=dark" width="102%" height="75%" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+          <iframe
+            src="https://discord.com/widget?id=1074365724400832532&theme=dark"
+            width="102%"
+            height="75%"
+            allowtransparency="true"
+            frameborder="0"
+            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+          ></iframe>
         </div>
         <section>
           <div className="line">
