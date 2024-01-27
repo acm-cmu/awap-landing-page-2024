05d8a6afe5c59ce77dfd06a55bed853a0861d5e1
Neeha Kurelli <62197412+nkurelli@users.noreply.github.com>
2024-01-27 01:25:47 -0500

    schedule

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,28 +1,148 @@
-import "../styles/Register.css";
+import "../styles/Schedule.css";
+import ReactDOMServer from "react-dom/server";
 import rover from "../rover.png";
+const fridaySchedule = () => {
+  return (
+    <div>
+      <div class="schedule-event main-event2">
+        <div class="col">
+          <p>5:00 - 6:00 PM</p>
+        </div>
+        <div class="col">
+          <p>Check-in</p>
+          <p class="details">Rashid Auditorium</p>
+          <p class="details">Check in with organizers and find team members</p>
+        </div>
+      </div>
+      <div class="schedule-event main-event2">
+        <div class="col">
+          <p>6:00 - 6:30 PM</p>
+        </div>
+        <div class="col">
+          <p>Opening Ceremony</p>
+          <p class="details">Rashid Auditorium</p>
+          <p class="details">Join us for the opening ceremony of AWAP 2024!</p>
+        </div>
+      </div>
+      <div class="schedule-event foods">
+        <div class="col">
+          <p>7:00 - 8:00 PM</p>
+        </div>
+        <div class="col">
+          <p>Dinner</p>
+          <p class="details">Reddy Conference Room</p>
+          <p class="details">Enjoy some delicious food to kickstart your coding!</p>
+        </div>
+      </div>
+      <div class="schedule-event foods">
+        <div class="col">
+          <p>12:00 - 1:00 AM</p>
+        </div>
+        <div class="col">
+          <p>Midnight Snack</p>
+          <p class="details">Gates 3 Atrium</p>
+          <p class="details">Insomnia Cookies</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const saturdaySchedule = () => {
+  return (
+    <div>
+      <div class="schedule-event foods">
+        <div class="col">
+          <p>8:00 - 10:00 AM</p>
+        </div>
+        <div class="col">
+          <p>Breakfast</p>
+          <p class="details">Reddy Conference Room</p>
+          <p class="details">Get breakfast after a long night of coding!</p>
+        </div>
+      </div>
+      <div class="schedule-event foods">
+        <div class="col">
+          <p>12:00 - 1:00 PM</p>
+        </div>
+        <div class="col">
+        <p class="details">Reddy Conference Room</p>
+          <p>Lunch</p>
+          <p class="details">Stop by for some fuel before your last few submissions!</p>
+        </div>
+      </div>
+      <div class="schedule-event main-event2">
+        <div class="col">
+          <p>2:00 PM</p>
+        </div>
+        <div class="col">
+          <p>Coding Stops</p>
+        </div>
+      </div>
+      <div class="schedule-event main-event2">
+        <div class="col">
+          <p>5:00 PM</p>
+        </div>
+        <div class="col">
+          <p>Closing Ceremony</p>
+          <p class="details">Rashid Auditorium</p>
+          <p class="details">See how your bot plays against other players!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function showFriday() {
+  document.getElementById("friday").style.border = "3px solid white";
+  document.getElementById("saturday").style.border = "0px solid white";
+  document.getElementById("schedule").innerHTML =
+    ReactDOMServer.renderToStaticMarkup(fridaySchedule());
+}
+
+function showSaturday() {
+  document.getElementById("friday").style.border = "0px solid white";
+  document.getElementById("saturday").style.border = "3px solid white";
+  document.getElementById("schedule").innerHTML =
+    ReactDOMServer.renderToStaticMarkup(saturdaySchedule());
+}
 
 const EventDetails = () => {
   return (
-    <div className="register">
-      <div className="register-content">
-        <div className="section-header">Event Details</div>
-        <p className="register-update">
-          {/* We're still finalizing the details for AWAP 2024; stay tuned for
-          updates! */}
-        </p>
-        <div className="register-box">
-          <p className="register-description">
-            Start: January 27, 5 PM @ Rashid Auditorium<br></br>
-            End: January 28, 5 PM @ Rashid Auditorium<br></br>
-          </p>
-          <iframe src="https://discord.com/widget?id=1099870874594705539&theme=dark" width="300" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-        </div>
-        <section>
-          <div className="line">
-            <hr />
+    <div className="schedule-page">
+      <div className="schedule-content">
+        <div className="schedule-header"></div>
+        <div className="section-header">Schedule</div>
+        <div class="legend">
+          <div class="legend-event">
+            <div class="legend-box foods"></div>
+            <p class="legend-label">Food</p>
           </div>
-          <img className="rover" src={rover} alt="" />
-        </section>
+          <div class="legend-event">
+            <div class="legend-box main-event2"></div>
+            <p class="legend-label">Main Event</p>
+          </div>
+          <div class="legend-event">
+            <div class="legend-box workshop"></div>
+            <p class="legend-label">Workshop</p>
+          </div>
+        </div>
+        <div class="schedule-days">
+          <button class="year-button" id="friday" onClick={showFriday}>
+            <span class="schedule-header-word">FRIDAY 9/15</span>
+          </button>
+          <button
+            class="year-button"
+            id="saturday"
+            onClick={showSaturday}
+          >
+            <span class="schedule-header-word">SATURDAY 9/16</span>
+          </button>
+        </div>
+        <div class="schedule" id="schedule">
+          {fridaySchedule()}
+        </div>
+        <div className="container-after"></div>
       </div>
     </div>
   );
